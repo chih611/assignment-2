1a9b60d4c88bd074d71ba8464ca4c2156e993696
halubuntu <halubuntu@hal.myguest.virtualbox.org>
2023-10-15 17:54:08 +1100

    create phones apis

--- api/controllers/phone.controller.js
+++ api/controllers/phone.controller.js
@@ -3,26 +3,58 @@ const Phones = db.phones;
 const Op = db.Sequelize.Op;
 
 // Create phone
-exports.create = (req, res) => {
-    
+exports.create = ({ params, body }, res) => {
+    Phones.create({
+        phone_name: body.name,
+        phone_no: body.no,
+        contact_id: params.contactId
+    })
+        .then(data => res.send(data))
+        .catch(message => res.send(message));
 };
 
 // Get all phones
 exports.findAll = (req, res) => {
-    
+    Phones.findAll()
+        .then(data => res.send(data))
+        .catch(message => res.send(message));
 };
 
 // Get one phone by id
-exports.findOne = (req, res) => {
-  
+exports.findOne = ({ params }, res) => {
+    Phones.findOne({
+        where: { id: params.phoneId }
+    })
+        .then(data => res.send(data))
+        .catch(message => res.send(message));
 };
 
 // Update one phone by id
-exports.update = (req, res) => {
-    
+exports.update = ({ body, params }, res) => {
+    Phones.update({
+        phone_name: body.name,
+        phone_no: body.no
+    }, { where: { id: params.phoneId } })
+        .then(data => res.send(data ? data : 'No data updated!'))
+        .catch(message => res.send(message));
 };
 
 // Delete one phone by id
+exports.delete = ({ params }, res) => {
+    Phones.destroy({
+        where: {
+            id: params.contactId
+        }
+    })
+        .then(() => res.send())
+        .catch(message => res.send(message));
+};
+
+// Delete all phones
 exports.delete = (req, res) => {
-    
+    Phones.destroy({
+        where: {}
+    })
+        .then(() => res.send())
+        .catch(message => res.send(message));
 };
